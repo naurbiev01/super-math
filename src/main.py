9e4c0583cf1b3974c9f71d3e6e11d081c7ce7f5b
naurbiev02 <anaurbiev1124@mail.ru>
2025-01-12 16:14:19 +0300

    Updated this file  main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 import modules.perimetr as perimetr
+import modules.factorial as factorial
+
+f1 = factorial.fact_normal(5)_
+print(f1)
+
+f2 = factorial.fact_normal(13)_
+print(f2)
 
 p1 = perimetr.perimetr_func([1, 2, 5])
 print(p1)
@@ -6,7 +13,7 @@ print(p1)
 p2 = perimetr.perimetr_func([1, 2, 5, 7, 6])
 print(p2)
 
-print("Hello Wirld")
+print("Hello World")
 
 
 
